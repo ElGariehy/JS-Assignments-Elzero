ee94320c8a2980b85e92f0000e28c311913485a6
AlGaraiehy <algaraiehyemad2901@gmail.com>
2023-06-02 20:19:40 +0300

    assignment 10-to-17

---
+++ assign10-17/js/main.js
@@ -0,0 +1,37 @@
+let numberOne = 10, numberTwo = 20;
+console.log(numberOne +""+ numberTwo)
+console.log(typeof numberOne +" "+  typeof numberTwo)
+console.log(`${numberOne}${numberTwo} `)
+console.log(`${typeof numberOne} ${typeof numberTwo} `)
+console.log(numberTwo +"\n"+ numberOne)
+console.log(`${numberTwo}
+${numberOne}`)
+
+
+// ---------------التكليف 02 -----------------
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+
+// ---------------التكليف 03 -----------------
+console.log("\`I'm In\n\
+\\\\\n\
+Love \\\\ \"\"\" '\'\'\n\
+++ With +\+\n\
+\\\"\"\"\\\"\"\"\n\
+\"\"JavaScript\"\"``")
+
+
+// ---------------التكليف 04 -----------------
+let a = 21;
+let b = 20;
+
+console.log("_" + (a + "_" + b).repeat(4) + "_"); // _21_2021_2021_2021_20_
+
+
+
+
+
+
+
+
